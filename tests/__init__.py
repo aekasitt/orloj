e422d4fd9f004eb69cc776c1c4bdce7d480dfc86
Sitt Guruvanich <aekazitt@gmail.com>
2024-05-18 22:55:58 +0700

    Write countdown test for OrlojMiddleware

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,31 +12,68 @@
 """Module defining test_client fixture used by test suites under tests/ directory."""
 
 ### Standard Packages ###
-from pytest import fixture
+from contextlib import asynccontextmanager
+from os import path, remove
+from pickle import PickleError, dump, load
+from typing import AsyncIterator, Generator
 
 ### Third-Party Packages ###
 from fastapi import Depends, FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse, Response
 from fastapi.testclient import TestClient
+from pytest import fixture
 
 ### Local Modules ###
 from orloj import OrlojMiddleware
 
+TEST_PICKLE: str = "tests/orloj.pickle"
 
-@fixture
-def test_client() -> TestClient:
-  """
-  Sets up a FastAPI TestClient with OrlojMiddleware updating local pickle file created and
-  destroyed by lifespan asynccontextmanager.
 
-  ---
-  :returns: TestClient
-  """
-  app = FastAPI()
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+  countdown: int = 10
+  if path.exists(TEST_PICKLE):
+    remove(TEST_PICKLE)
+  with open(TEST_PICKLE, "wb") as pickle_file:
+    dump(countdown, pickle_file)
+  yield
+  if path.exists(TEST_PICKLE):
+    remove(TEST_PICKLE)
 
-  # TODO: create lifespan asynccontextmanager and two endpoints
-  # 1: /reset - resets the clock and start state at 10
-  # 2: /state - get countdown state
-  # with the orlojmiddleware counting down from ten and saving state to pickle-file
-  # read only by /state endpoint
-  return TestClient(app)
+
+app: FastAPI = FastAPI(lifespan=lifespan)
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def state() -> str:
+  try:
+    with open(TEST_PICKLE, "rb") as pickle_file:
+      return str(load(pickle_file))
+  except FileNotFoundError:
+    return "File not found"
+
+
+@app.put("/", response_class=PlainTextResponse)
+async def reset(response: Response) -> str:
+  countdown: int = 10
+  try:
+    with open(TEST_PICKLE, "wb") as pickle_file:
+      dump(countdown, pickle_file)
+  except PickleError:
+    response.status_code = 500
+    return "Not OK"
+  return "OK"
+
+
+def wind_down() -> None:
+  try:
+    with open(TEST_PICKLE, "rb") as pickle_file:
+      countdown: int = load(pickle_file) - 1
+    with open(TEST_PICKLE, "wb") as pickle_file:
+      dump(countdown, pickle_file)
+  except PickleError:
+    ...
+
+
+app.add_middleware(OrlojMiddleware, interval=1, job=wind_down, job_id="wind-down")
+
+__all__ = ("TEST_PICKLE", "app")
---
+++ tests/countdown.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.9
+# coding:utf-8
+# Copyright (C) 2023-2024 All rights reserved.
+# FILENAME:    ~~/tests/countdown.py
+# VERSION:     0.1.3
+# CREATED:     2024-05-18 21:08
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from time import sleep
+
+### Third-party packages ###
+from fastapi.testclient import TestClient
+from httpx import Response
+
+### Local modules ###
+from tests import app
+
+
+def test_countdown() -> None:
+  with TestClient(app) as test_client:
+    response: Response = test_client.put("/")
+    assert response.text == "OK"
+    response = test_client.get("/")
+    assert response.text == "10"
+    sleep(1)
+    response = test_client.get("/")
+    assert response.text == "9"
+    sleep(1)
+    response = test_client.get("/")
+    assert response.text == "8"
+    response = test_client.put("/")
+    assert response.text == "OK"
+    response = test_client.get("/")
+    assert response.text == "10"
